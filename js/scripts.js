bba8e9b792558a02a17f5f7b8f2c8e792f10d1da
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-17 11:50:54 -0700

    WIP getting HTML and jquery to talk to one another

--- js/scripts.js
+++ js/scripts.js
@@ -23,8 +23,23 @@ Pizza.prototype.Cost = function (cost) {
   return cost;
 }
 
+// let orders = new Orders();
+// let myPizza = new Pizza(["Extra Cheese", "Pepperoni", "Onions"], "Large");
+// myPizza.Cost();
+// orders.addPizza(myPizza);
+// orders.pizzas;
+
 let orders = new Orders();
-let myPizza = new Pizza(["Extra Cheese", "Pepperoni", "Onions"], "Large");
-myPizza.Cost();
-orders.addPizza(myPizza);
-orders.pizzas;
+
+$(document).ready(function() {
+  $("form#new-order").submit(function(event) {
+    event.preventDefault();
+    const inputtedExtraCheese = $("input#extra-cheese").val();
+    const inputtedPepperoni = $("input#pepperoni").val();
+    const inputtedOnions = $("input#onions").val();
+    const inputtedSize = $("select#pizza-size").val();
+    let newPizza = new Pizza(inputtedExtraCheese, inputtedPepperoni, inputtedOnions, inputtedSize)
+    orders.addPizza(newPizza);
+    console.log(orders.addPizza)
+  });
+});
